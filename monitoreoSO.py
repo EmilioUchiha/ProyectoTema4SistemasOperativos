f4e8e305c336773e33e647a4a1d42f39e7caa632
Emilio Ramirez <emilioramirez1905@gmail.com>
2026-04-25 21:17:45 -0600

    Se agrego el monitoreoSO

--- monitoreoSO.py
+++ monitoreoSO.py
@@ -1,13 +1,15 @@
 import psutil as ps
 import time
+import random as rd
 
 class Proceso:
-    def __init__(self, pid, proceso,uso,memoria):
+    def __init__(self, pid, proceso,uso,memoria,tiempo):
         self.pid = pid
         self.proc = proceso
         self.usage = uso
         self.memory = memoria
-        self.estado = 'Ejecutandose'
+        self.tiempoRestante = tiempo
+        self.estado = 'LISTO'
 
 def obtener_informacion_sistema():
     print("Información del Sistema:")
@@ -15,27 +17,38 @@ def obtener_informacion_sistema():
     print(f"Memoria: {ps.virtual_memory().percent}%")
     print(f"Disco: {ps.disk_usage('/').percent}%")
         
-def carga_procesos():
+def carga_procesos(cantidad):
     procesos = []
     for proc in ps.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent']):
-        procesos.append(Proceso(proc.info['pid'], proc.info['name'], proc.info['cpu_percent'], proc.info['memory_percent']))
+        procesos.append(Proceso(proc.info['pid'], proc.info['name'], proc.info['cpu_percent'], proc.info['memory_percent'], rd.randint(15, 25)))
+        if len(procesos) >= cantidad:
+            break
     return procesos
 
-def obtener_informacion_procesos():
+def obtener_informacion_procesos(procesos):
     print("Información de Procesos:")
-    procesos = carga_procesos()
     for proc in procesos:
-        print(f"PID: {proc.pid} | Nombre: {proc.proc} | Uso: {proc.usage}% | Memoria: {round(proc.memory, 2)}% | Estado: {proc.estado} ")
+        print(f"PID: {proc.pid} | Nombre: {proc.proc} | Uso: {proc.usage}% | Memoria: {round(proc.memory, 2)}% | Estado: {proc.estado} | Tiempo Restante: {proc.tiempoRestante} [s]")
 
-def cambio_de_contexto():
-    pass
+def cambio_de_contexto(procesos, quantum):
+    print(f'Cambio de Proceso\n')
+    colaprocesos = procesos.copy()
+    while colaprocesos:
+        proc = colaprocesos.pop()
+        proc.estado = 'EN PROCESO'
+    
+    
 
 def main():
     print("Monitoreo del Sistema Operativo\n")
     obtener_informacion_sistema()
     print("\n")
-    obtener_informacion_procesos()
-
+    cantidad_procesos = int(input("Ingrese la cantidad de procesos a mostrar: "))
+    procesos = carga_procesos(cantidad_procesos)
+    quantum = int(input("Ingrese el quantum para el cambio de contexto (en segundos): "))
+    obtener_informacion_procesos(procesos)
+    print("\n")
+    cambio_de_contexto(procesos,quantum)
 if __name__ == "__main__":
     main()
     
